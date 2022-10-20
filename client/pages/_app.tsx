35dde76917e8335f5bd6d0a0713766124f74d842
mramirid <amir.muh.hakim@gmail.com>
2022-10-20 10:05:46 +0700

    feat(client): add homepage

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { init as initAOS } from "aos";
+import "aos/dist/aos.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import { useEffect } from "react";
+import "../styles/utilities.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    initAOS();
+  }, []);
+
+  return (
+    <>
+      {/* Bootstrap JS */}
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossOrigin="anonymous"
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
-
-export default MyApp
