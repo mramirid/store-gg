8fd34596a6b6caee8d9a6734e602937a63635a6a
mramirid <amir.muh.hakim@gmail.com>
2022-11-11 10:19:03 +0700

    fix(client): remove unwanted semicolon

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -18,7 +18,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
         crossOrigin="anonymous"
       />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
