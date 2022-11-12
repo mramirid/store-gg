98728f38349bb1559aaa6494055592bb27dd6eb4
mramirid <amir.muh.hakim@gmail.com>
2022-11-12 13:54:28 +0700

    fix(backend): improve express error handling

--- backend/app/admin.router.ts
+++ backend/app/admin.router.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import createHttpError from "http-errors";
 import { StatusCodes } from "http-status-codes";
 import categoriesRouter from "./categories/router";
 import homeRouter from "./home/router";
@@ -7,9 +6,15 @@ import homeRouter from "./home/router";
 const adminRouter = express.Router();
 
 adminRouter.use("/", homeRouter);
+
 adminRouter.use("/categories", categoriesRouter);
 
-adminRouter.use((_, __, next) => next(new createHttpError.NotFound()));
+adminRouter.use((_, res) => {
+  res.status(StatusCodes.NOT_FOUND).render("admin/404", {
+    pageTitle: "404 Not Found",
+    statusCode: StatusCodes.NOT_FOUND,
+  });
+});
 
 const errorHandler: express.ErrorRequestHandler = (error, req, res, next) => {
   if (req.app.get("env") === "development") {
@@ -17,20 +22,10 @@ const errorHandler: express.ErrorRequestHandler = (error, req, res, next) => {
     return;
   }
 
-  if (
-    createHttpError.isHttpError(error) &&
-    error.status === StatusCodes.NOT_FOUND
-  ) {
-    res.status(error.status).render("admin/404", {
-      pageTitle: "404 Error Page",
-      statusCode: error.status,
-    });
-    return;
-  }
-
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).render("admin/5XX", {
-    pageTitle: "500s Error Page",
-    statusCode: error.status ?? StatusCodes.INTERNAL_SERVER_ERROR,
+  const statusCode = error.status ?? StatusCodes.INTERNAL_SERVER_ERROR;
+  res.status(statusCode).render("admin/error", {
+    pageTitle: "Error Page",
+    statusCode,
   });
 };
 adminRouter.use(errorHandler);
