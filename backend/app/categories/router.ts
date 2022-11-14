80a7e6095d0038418ce915a3ceb7e7da199b401b
mramirid <amir.muh.hakim@gmail.com>
2022-11-14 13:28:27 +0700

    refactor(backend): destructure import

--- backend/app/categories/router.ts
+++ backend/app/categories/router.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import { Router } from "express";
 import controller from "./controller";
 
-const categoriesRouter = express.Router();
+const categoriesRouter = Router();
 
 categoriesRouter.get("/", controller.viewCategories);
 categoriesRouter.get("/create", controller.viewCreateCategory);
