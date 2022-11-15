099dccc5be212967cdaf2a7b689d15fb03ebe712
mramirid <amir.muh.hakim@gmail.com>
2022-11-15 09:00:06 +0700

    refactor(backend): update rupiah formatter

--- client/utils/format.ts
+++ client/utils/format.ts
@@ -2,6 +2,6 @@ export function formatIDR(amount: number) {
   return amount.toLocaleString("id-ID", {
     style: "currency",
     currency: "IDR",
-    maximumFractionDigits: 0,
+    // maximumFractionDigits: 0,
   });
 }
