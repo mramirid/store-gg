34216cec52cf055a648489ecab1f60f69cbffa38
mramirid <amir.muh.hakim@gmail.com>
2022-11-26 21:34:35 +0700

    feat(backend): add homepage

--- backend/app/home/controller.ts
+++ backend/app/home/controller.ts
@@ -1,7 +1,38 @@
 import type express from "express";
+import Category from "../categories/model";
+import Transaction from "../transactions/model";
+import Voucher from "../vouchers/model";
 
 export default { viewHome };
 
-function viewHome(_: express.Request, res: express.Response) {
-  res.render("admin", { pageTitle: "Home" });
+async function viewHome(
+  _: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  let numCategories: number,
+    numVouchers: number,
+    numMembers: number,
+    numTransactions: number;
+
+  try {
+    [numCategories, numVouchers, numMembers, numTransactions] =
+      await Promise.all([
+        Category.countDocuments(),
+        Voucher.countDocuments(),
+        Promise.resolve(NaN),
+        Transaction.countDocuments(),
+      ]);
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  res.render("admin", {
+    pageTitle: "Home",
+    numCategories,
+    numVouchers,
+    numMembers,
+    numTransactions,
+  });
 }
