cc3b2dac1bad57955d0a03839144aa373a947dad
mramirid <amir.muh.hakim@gmail.com>
2022-11-28 09:38:16 +0700

    feat(backend): improve api error handling

--- backend/app/member.router.ts
+++ backend/app/member.router.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express from "express";
 import createHttpError from "http-errors";
-import { StatusCodes } from "http-status-codes";
+import { getReasonPhrase, StatusCodes } from "http-status-codes";
 import mongoose from "mongoose";
 import { getErrorMessage, joinFormErrorMessages } from "../utils/error";
 import memberHomeController from "./homes/member.controller";
@@ -14,10 +14,12 @@ memberRouter.use(express.json());
 
 memberRouter.use("/homepage", memberHomeController.getHomepageData);
 
-memberRouter.use((_, __, next) => next(createHttpError(404)));
+memberRouter.use((_, __, next) =>
+  next(new createHttpError.NotFound("Page not found!"))
+);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const errorHandler: express.ErrorRequestHandler = (error, _, res, __) => {
+const errorHandler: express.ErrorRequestHandler = (error, req, res, __) => {
   if (error instanceof mongoose.Error.ValidationError) {
     res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
       message: joinFormErrorMessages(error.errors),
@@ -26,14 +28,25 @@ const errorHandler: express.ErrorRequestHandler = (error, _, res, __) => {
     return;
   }
 
+  const isDevMode = req.app.get("env") === "development";
+
   if (createHttpError.isHttpError(error)) {
-    res.status(error.status).json({ message: error.message });
+    res.status(error.status).json({
+      message:
+        error.expose || isDevMode
+          ? error.message
+          : getReasonPhrase(error.status),
+    });
     return;
   }
 
   res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ message: getErrorMessage(error) });
+    .json({
+      message: isDevMode
+        ? getErrorMessage(error)
+        : getReasonPhrase(error.status),
+    });
 };
 memberRouter.use(errorHandler);
 
