6904a9f3f08995159ccb37fcddb1ac61952126c2
mramirid <amir.muh.hakim@gmail.com>
2022-12-06 14:26:04 +0700

    feat(backend): implement voucher checkout api

--- backend/app/vouchers/client.controller.ts
+++ backend/app/vouchers/client.controller.ts
@@ -1,34 +1,133 @@
 import type express from "express";
 import createHttpError from "http-errors";
 import { StatusCodes } from "http-status-codes";
+import mongoose, { MergeType } from "mongoose";
+import { CustomValidationError } from "../../lib/error";
+import type { BankDoc } from "../banks/model";
+import Bank from "../banks/model";
 import type { CategoryDoc } from "../categories/model";
+import type { MemberDoc } from "../members/model";
 import type { NominalDoc } from "../nominals/model";
+import Nominal from "../nominals/model";
+import type { PaymentMethodDoc } from "../payment-methods/model";
+import PaymentMethod from "../payment-methods/model";
+import Transaction from "../transactions/model";
 import Voucher, { VoucherDoc } from "./model";
 
-export default { getVoucher };
+export default { getVoucher, checkout };
 
-type PopulatedVoucherDoc = Omit<VoucherDoc, "category" | "nominals"> & {
-  category: Pick<CategoryDoc, "_id" | "name">;
-  nominals: Pick<NominalDoc, "_id" | "name" | "quantity" | "price">[];
-};
+const voucher404Error = new createHttpError.NotFound("Voucher not found");
 
 async function getVoucher(
   req: express.Request<{ id: string }>,
   res: express.Response,
   next: express.NextFunction
 ) {
+  type PopulatedVoucherDoc = MergeType<
+    VoucherDoc,
+    {
+      category: Pick<CategoryDoc, "_id" | "name">;
+      nominals: Array<Pick<NominalDoc, "_id" | "name" | "quantity" | "price">>;
+    }
+  >;
   let voucher: PopulatedVoucherDoc;
 
   try {
     voucher = await Voucher.findById(req.params.id)
-      .orFail(new createHttpError.NotFound("Voucher not found"))
+      .orFail(voucher404Error)
       .select("name category nominals imageName")
-      .populate("category", "name")
-      .populate("nominals", "name quantity price");
+      .populate<Pick<PopulatedVoucherDoc, "category">>("category", "name")
+      .populate<Pick<PopulatedVoucherDoc, "nominals">>(
+        "nominals",
+        "name quantity price"
+      );
   } catch (error) {
     next(error);
     return;
   }
 
   res.status(StatusCodes.OK).json({ voucher });
+}
+
+type CheckoutReqBody = {
+  nominalId: string;
+  paymentMethodId: string;
+  bankId: string;
+  member: {
+    bankAccountName: string;
+    gameId: string;
+  };
+};
+
+async function checkout(
+  req: express.Request<{ id: string }, unknown, CheckoutReqBody>,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  type PopulatedVoucherDoc = MergeType<VoucherDoc, { category: CategoryDoc }>;
+  let voucher: PopulatedVoucherDoc,
+    nominal: NominalDoc,
+    paymentMethod: PaymentMethodDoc,
+    bank: BankDoc;
+
+  try {
+    [voucher, nominal, paymentMethod, bank] = await Promise.all([
+      Voucher.findById(req.params.id)
+        .orFail(voucher404Error)
+        .populate<Pick<PopulatedVoucherDoc, "category">>("category"),
+      Nominal.findById(req.body.nominalId).orFail(
+        new createHttpError.NotFound("Nominal not found")
+      ),
+      PaymentMethod.findById(req.body.paymentMethodId).orFail(
+        new createHttpError.NotFound("Payment method not found")
+      ),
+      Bank.findById(req.body.bankId).orFail(
+        new createHttpError.NotFound("Bank not found")
+      ),
+    ]);
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  try {
+    await Transaction.create({
+      voucher: {
+        name: voucher.name,
+        imageName: voucher.imageName,
+      },
+      category: {
+        current: voucher.category.id,
+        name: voucher.category.name,
+      },
+      nominal: {
+        name: nominal.name,
+        price: nominal.price,
+        quantity: nominal.quantity,
+      },
+      paymentMethod: paymentMethod.name,
+      targetBank: {
+        name: bank.name,
+        holderName: bank.holderName,
+        holderNumbers: bank.holderNumbers,
+      },
+      taxRate: 10 / 100,
+      member: {
+        current: (req.user as MemberDoc).id,
+        bankAccountName: req.body.member.bankAccountName,
+        gameId: req.body.member.gameId,
+      },
+    });
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      const validationError = new CustomValidationError(error);
+      validationError.status = StatusCodes.UNPROCESSABLE_ENTITY;
+      next(validationError);
+    } else {
+      next(error);
+    }
+    return;
+  }
+
+  res.status(StatusCodes.CREATED).json({ message: "Checkout success" });
 }
--- backend/app/vouchers/client.router.ts
+++ backend/app/vouchers/client.router.ts
@@ -1,16 +1,14 @@
-import { Response, Router } from "express";
+import { Router } from "express";
 import memberMiddleware from "../members/middleware";
 import controller from "./client.controller";
 
 const clientVouchersRouter = Router();
 
 clientVouchersRouter.get("/:id", controller.getVoucher);
-clientVouchersRouter.get(
+clientVouchersRouter.post(
   "/:id/checkout",
   memberMiddleware.ensureAuthenticated,
-  (_: unknown, res: Response) => {
-    res.status(200).json({ message: "Authenticated" });
-  }
+  controller.checkout
 );
 
 export default clientVouchersRouter;
